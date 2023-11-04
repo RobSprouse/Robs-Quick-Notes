4c24188cf430cca0752d975cfb3a37c32ff75b5f
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-11-04 15:53:51 -0400

    corrected the response to the api/notes
    corrected the response to the api/notes to meet the requirements

--- Express.js
+++ Express.js
@@ -1,14 +1,9 @@
-// [x]: Application front end must connect to an Express.js back end.
-
-// [x]: Application back end must store notes with unique IDs in a JSON file.
-
-// [ ]: Application must be deployed to Heroku.
-
 // COMMENT: Dependencies
 import express from "express";
 import { readFile, writeFile } from "fs/promises";
 import { nanoid } from "nanoid";
 
+// COMMENT: Constant declarations
 const notesJSON = "./db/db.json";
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,20 +15,15 @@ app.use(express.urlencoded({ extended: true }));
 // COMMENT: Middleware for serving all the static files in the public folder
 app.use(express.static("public"));
 
-// [x]: * `GET /notes` should return the `notes.html` file.
 // COMMENT: GET Route for notes page
 app.get("/notes", (req, res) => {
      res.sendFile("./public/notes.html", { root: "." });
 });
 
-// [x]: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
      res.sendFile(notesJSON, { root: "." });
 });
 
-// [x]: `POST /api/notes` should receive a new note to save on the request body, add it to
-// the `db.json` file, and then return the new note to the client. You'll need to find a way
-// to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post("/api/notes", async (req, res) => {
      const newNote = req.body;
      newNote.id = nanoid();
@@ -42,16 +32,13 @@ app.post("/api/notes", async (req, res) => {
           const notes = JSON.parse(notesDb);
           notes.push(newNote);
           await writeFile(notesJSON, JSON.stringify(notes, null, 2));
-          res.status(201).json({ message: "Note created successfully" });
+          res.status(201).json(newNote);
      } catch (err) {
           console.error(err, "API Failed, please try again.");
           res.status(500).json({ message: "API Failed, please try again." });
      }
 });
 
-// [x]:  As a bonus: `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete.
-// To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the
-// given `id` property, and then rewrite the notes to the `db.json` file.
 // COMMENT: DELETE Route for deleting a note
 app.delete("/api/notes/:id", async (req, res) => {
      const id = req.params.id;
@@ -66,7 +53,7 @@ app.delete("/api/notes/:id", async (req, res) => {
                return res.status(404).json({ message: "Note not found!" });
           }
           await writeFile(notesJSON, JSON.stringify(updatedNotes, null, 2));
-          res.status(200).json({ message: "Note successfully deleted!" });
+          res.status(200).json({ message: "Deleted" });
      } catch (err) {
           console.error(err, "API Failed, please try again.");
           res.status(500).json({ message: "API Failed, please try again." });
